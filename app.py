fd11b09cf6ece3fa3ec006585b79e5cfac7bf2f7
Agnija Berzina <agni.berzina@gmail.com>
2026-04-01 00:21:22 +0100

    Connected PostgreSQL database

--- app.py
+++ app.py
@@ -1,41 +1,54 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-todos = [
-    {'id': 1, 'name': "Learn about CORS", 'checked': False},
-    {'id': 2, 'name': "Learn about middleware", 'checked': False}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    checked = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        task = request.form.get('task', '').strip()
-        if task:
-            todos.append({
-                'id': len(todos) + 1,
-                'name': task,
-                'checked': False
-            })
+        task_name = request.form.get('task')
+        if task_name:
+            new_task = Todo(name=task_name)
+            db.session.add(new_task)
+            db.session.commit()
         return redirect(url_for('home'))
-
+    
+    todos = db.session.scalars(select(Todo).order_by(Todo.checked, Todo.id)).all()
     return render_template('index.html', items=todos)
 
 @app.route('/checked/<int:task_id>', methods=['POST'])
 def toggle_checked(task_id):
-    for todo in todos:
-        if todo['id'] == task_id:
-            todo['checked'] = not todo['checked']
-            break
+    task = db.session.get(Todo, task_id)
+    if task:
+        task.checked = not task.checked
+        db.session.commit()
     return redirect(url_for('home'))
 
 @app.route('/delete/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
-    global todos
-    for todo in todos:
-        if todo['id'] == task_id:
-            todos.remove(todo)
+    task = db.session.get(Todo, task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
     return redirect(url_for('home'))
 
 
